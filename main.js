7e719b587755382fa392893b1888dc3759be78d3
kyorotyan <kakosa45@gmail.com>
2025-10-21 01:25:17 +0900

    enterを押した場合の処理を追加

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 /* document.querySelector()で入力フォームの要素を取得し、定数を宣言 */
 /* 定数は const 変数は let */
 const addTask = document.querySelector('.add');
+
+/*
+    enterが押されたタイミングを感知するため、EventTarget.addEventListener(type, listener) メソッドを使用
+    第一引数のtypeにはイベントの種類を表す文字列。第二引数のlistenerにはイベント発生時に実行する関数を使用
+*/
+addTask.addEventListener('submit', e => {
+    /* e.preventDefault() メソッドはデフォルトのアクションをキャンセル */
+    e.preventDefault()
+    const text = addTask.addEventListener.value.trim();
+    if (text != '') {
+    }
+});
