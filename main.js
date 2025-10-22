375fdbd357fa8daf63ef355c8e1f8c18e7aaee18
kyorotyan <kakosa45@gmail.com>
2025-10-23 01:03:41 +0900

    現在日時に関するデータとTodoタスクの値を含んだオブジェクトを作成し、配列に格納する処理を追加

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 /* 定数は const 変数は let */
 const addTask = document.querySelector('.add');
 
+/* 空の配列が入ったtodosという変数を宣言 */
+let todos = [];
+
+function addTodo(text) {
+    const todo = {
+        id: Date.now(),
+        text,
+    };
+    /* 配列に追加 */
+    todos.push(todo)
+}
+
 /*
     enterが押されたタイミングを感知するため、EventTarget.addEventListener(type, listener) メソッドを使用
     第一引数のtypeにはイベントの種類を表す文字列。第二引数のlistenerにはイベント発生時に実行する関数を使用
@@ -9,7 +21,9 @@ const addTask = document.querySelector('.add');
 addTask.addEventListener('submit', e => {
     /* e.preventDefault() メソッドはデフォルトのアクションをキャンセル */
     e.preventDefault()
+    /* .trim()は文字列の両端の空白を削除するメソッド */
     const text = addTask.addEventListener.value.trim();
+    /* 入力値がある場合のみ処理に通すようにする */
     if (text != '') {
     }
 });
